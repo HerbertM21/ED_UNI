42a851e928d75a4f99e64e5d000d6a6b02486e8c
HerbertM21 <herbertmm12@gmail.com>
2023-05-17 15:23:38 -0400

    Complejidades añadidas

--- 01_Ejercicios_c/ARREGLOS/max_arreglo.c
+++ 01_Ejercicios_c/ARREGLOS/max_arreglo.c
@@ -3,7 +3,7 @@
 int encontrarMaximo(int arreglo[], size_t tamano) {
     int maximo = arreglo[0]; // Suponemos que el primer elemento es el máximo inicialmente
     
-    for (int i = 1; i <= tamano; i++) {
+    for (int i = 1; i < tamano; i++) {
         if (arreglo[i] > maximo) {
             maximo = arreglo[i]; // Actualizamos el máximo si encontramos un valor mayor
         }
@@ -12,11 +12,24 @@ int encontrarMaximo(int arreglo[], size_t tamano) {
     return maximo;
 }
 
+int encontrarMinimo(int arreglo[], size_t tamano){
+    int minimo = arreglo[0];
+
+    for (int i = 1; i < tamano; i++) {
+        if (arreglo[i] < minimo) {
+            minimo = arreglo[i]; // Actualizamos el máximo si encontramos un valor mayor
+        }
+    }
+    
+    return minimo;    
+}
+
 int main() {
     int arreglo[] = {5, 8, 3, 1, 10, 7};
     size_t tamano = sizeof(arreglo) / sizeof(arreglo[0]);
     
     int maximo = encontrarMaximo(arreglo, tamano);
+    int minimo = encontrarMinimo(arreglo, tamano);
     
     printf("El máximo del arreglo es: %d\n", maximo);
     
--- 04_complejidad/Codigos complejidad/01_merge_sort_nlogn.md
+++ 04_complejidad/Codigos complejidad/01_merge_sort_nlogn.md
@@ -1,11 +1,8 @@
 # Aclaración complejidad Merge Sort (nLog(n))
 
-
-
 En cada iteración de la función `mergeSort`, el arreglo se divide en dos subarreglos de tamaño aproximadamente igual. Esto se hace recursivamente hasta que cada subarreglo tiene un solo elemento, y luego se combinan en orden creciente usando la función `merge`.
 
-La complejidad de tiempo de Merge Sort proviene de la cantidad de veces que se divide el arreglo en subarreglos y de la cantidad de trabajo que se realiza al combinar los subarreglos. 
-
+La complejidad de tiempo de Merge Sort proviene de la cantidad de veces que se divide el arreglo en subarreglos y de la cantidad de trabajo que se realiza al combinar los subarreglos.
 
 ### IMPORTANTE:
 
